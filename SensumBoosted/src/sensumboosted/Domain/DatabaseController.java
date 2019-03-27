87f3f0e29a5c09617779c8f46a27e86c697b2ee6
Mikkel Høyberg <mikkelhoyberg@gmail.com>
2019-03-27 07:59:15 +0100

    Test af linjer
    Test

--- SensumBoosted/src/sensumboosted/Domain/DatabaseController.java
+++ SensumBoosted/src/sensumboosted/Domain/DatabaseController.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Mikkel har lavet denne linje som test og skal fjernes igen
  */
 package sensumboosted.Domain;
 
