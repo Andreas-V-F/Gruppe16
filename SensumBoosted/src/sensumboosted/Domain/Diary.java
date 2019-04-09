a50d560da34f3b53a23d972b58e396c64feabb67
Lukas Andersen <andersenlukas@gmail.com>
2019-04-09 22:16:23 +0200

    Write to file + choose name for file

--- SensumBoosted/src/sensumboosted/Domain/Diary.java
+++ SensumBoosted/src/sensumboosted/Domain/Diary.java
@@ -5,10 +5,38 @@
  */
 package sensumboosted.Domain;
 
-/**
- *
- * @author Luka
- */
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Diary {
-    
+
+    public void createDiary() {
+        String fileName = getFileName("Enter output file name.");
+
+        File output = new File(fileName);
+
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(output, true));
+                 Scanner scan = new Scanner(System.in)) {
+            String line = "";
+            while (!line.equalsIgnoreCase("SAVE")) {
+                System.out.println("Write your diary (SAVE to end the log):");
+                line = scan.nextLine();
+                pw.println(line);
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Diary.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static String getFileName(String prompt) {
+        Scanner keyboard = new Scanner(System.in);
+        String fileName = null;
+        System.out.println(prompt);
+        fileName = keyboard.next();
+
+        return fileName;
+    }
 }
