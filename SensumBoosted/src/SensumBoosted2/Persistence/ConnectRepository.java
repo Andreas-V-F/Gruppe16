ed251503255a755a058833c6ae9480de907c84a6
Andreas Frederiksen <Andreas020299@gmail.com>
2019-05-07 14:24:20 +0200

    Create ConnectRepository.java

---
+++ SensumBoosted/src/SensumBoosted2/Persistence/ConnectRepository.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SensumBoosted2.Persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Andreas Frederiksen
+ */
+public class ConnectRepository {
+
+    private Connection connection = null;
+    private final String url = "jdbc:postgresql://balarama.db.elephantsql.com:5432/rsrrjzno";
+    private final String userDB = "rsrrjzno";
+    private final String passDB = "afVcwMqs2zGaNtod0axmHcsrAuy5u7uD";
+
+    public ConnectRepository() {
+        connect();
+    }
+    
+    public Connection connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            connection = DriverManager.getConnection(url, userDB, passDB);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    
+}
