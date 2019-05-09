5b3013d45bf4b256f294f345dcc474f1a822d0b0
ngPsych <43138865+ngPsych@users.noreply.github.com>
2019-05-09 02:51:51 +0200

    Update
    Problem explained.

--- SensumBoosted/src/SensumBoosted2/Persistence/CreateCitizenRepository.java
+++ SensumBoosted/src/SensumBoosted2/Persistence/CreateCitizenRepository.java
@@ -22,21 +22,28 @@ public class CreateCitizenRepository {
     Connection connection;
     ResultSet rs;
 
-    //VIRKER IKKE... FIND UD AF PROBLEM
+    // VIRKER IKKE... FIND UD AF PROBLEM
+    // Problemet er i databasen. Har noget med user_id SERIAL PIMARY KEY at gøre
+    // Eksempel: Når den skal oprette ny bruger, så vil den starte fra user_id = 1
+    // og fortælle at der allerede findes det user_id, og det gør sådan at programmet
+    // giver exception, da databasen melder fejl.
+    // Det blev testet i databasen og man skal køre den enkelte statement flere gange
+    // for at det når til det user_id der ikke findes og derfra vil den oprette ny bruger.
     public void createCitizenInformation(String firstname, String middlename, String lastname,
             int cpr, String address, int postalcode, String city, String email, int phonenumber,
             String department) {
         try {
             Statement st = connection.createStatement();
             String sql = "INSERT INTO citizen_information "
-                    + "(user_id, firstname, middlename, lastname, cpr, address, "
+                    + "(firstname, middlename, lastname, cpr, address, "
                     + "postal_code, city, email, phonenumber, department)"
-                    + " VALUES (" + ",'" + firstname + "','" + middlename
+                    + " VALUES (" + firstname + "','" + middlename
                     + "','" + lastname + "'," + cpr + ",'" + address + "'," + postalcode
                     + ",'" + city + "','" + email + "'," + phonenumber + ",'" + department + "')";
+
             st.executeUpdate(sql);
-            st.getGeneratedKeys();
-            st.close();
+            rs = st.getGeneratedKeys();
+            rs.close();
         } catch (SQLException ex) {
             Logger.getLogger(UserProfileRepository.class.getName()).log(Level.SEVERE, null, ex);
         }
