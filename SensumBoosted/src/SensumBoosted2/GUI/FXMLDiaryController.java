3ba65ef5bba6a1f6f161eece3d3eda319685f82a
Lukas Andersen <andersenlukas@gmail.com>
2019-05-09 21:44:45 +0200

    HEeej
    Co-Authored-By: aschj18 <aschj18@users.noreply.github.com>

--- SensumBoosted/src/SensumBoosted2/GUI/FXMLDiaryController.java
+++ SensumBoosted/src/SensumBoosted2/GUI/FXMLDiaryController.java
@@ -7,7 +7,16 @@ package SensumBoosted2.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,12 +25,61 @@ import javafx.fxml.Initializable;
  */
 public class FXMLDiaryController implements Initializable {
 
+    @FXML
+    private Pane diaryPane;
+    @FXML
+    private TextArea diaryTextField;
+    @FXML
+    private Button saveDiaryButton;
+    @FXML
+    private TableView<?> citizenTableView;
+    @FXML
+    private TableColumn<?, ?> citizenId;
+    @FXML
+    private TableColumn<?, ?> citizenName;
+    @FXML
+    private TableView<?> DiaryEntryTableView;
+    @FXML
+    private TableColumn<?, ?> text;
+    @FXML
+    private Button DeleteDiaryBTN;
+    @FXML
+    private Button editBTN;
+    @FXML
+    private Label nameDiaryLBL;
+    @FXML
+    private Label cprDiaryLBL;
+    @FXML
+    private Label emailDiaryLBL;
+    @FXML
+    private Label adresseDiaryLBL;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void saveDiaryButtonHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void backToCasePaneHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void dbClickRowHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void DeleteDiaryBTN(MouseEvent event) {
+    }
+
+    @FXML
+    private void editDiaryBTNHandler(ActionEvent event) {
+    }
+
 }
